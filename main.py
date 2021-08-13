7c2b6c99f5a6e7a6cccd0591ca388806580d6a40
ansoncpu <60058162+ansoncpu@users.noreply.github.com>
2021-08-13 12:57:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 
-browser.get('https://popdog.click/m?uuid=9ed6649f-0130-4982-b3b3-3ad690399d8f')
 browser.get('https://popdog.click')
 browser.find_element_by_id('settingsClose').click()
 cookie = browser.find_element_by_id('cookie')
